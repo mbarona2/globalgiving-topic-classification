e6dedb217af62cf8349c1b949b3a6df748e985fd
Melissa Barona <melissabarona4@gmail.com>
2022-08-03 21:38:51 -0400

    adding subfolders

---
+++ globalgiving-topic-classification/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project uses the GlobalGiving API to collect projects and classify them under a theme as an auto-tag application',
+    author='Melissa Barona',
+    license='MIT',
+)
